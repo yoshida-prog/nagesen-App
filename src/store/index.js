6be7208fa4f72dbc241ed1dc6721eeb069da181a
よしだ <56685063+yoshida-prog@users.noreply.github.com>
2021-02-02 00:50:53 +0900

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,25 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    username: '',
+    Email: '',
+    password: ''
+  },
+  getters: {
+    getUsername: state => state.username,
+    getEmail: state => state.Email,
+    getPassword: state => state.password
   },
   mutations: {
+    updateUsername(state, value){
+      state.username = value
+    },
+    updateEmail(state, value){
+      state.Email = value
+    },
+    updatePassword(state, value){
+      state.password = value
+    }
   },
   actions: {
   },
