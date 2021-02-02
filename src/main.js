dff408126e78ec19640a6a73cbe537ad5a21835c
よしだ <56685063+yoshida-prog@users.noreply.github.com>
2021-02-02 18:51:37 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,17 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import firebase from 'firebase'
+import { firebaseConfig } from './firebase/config'
 
 Vue.config.productionTip = false
 
-var firebaseConfig = {
-  apiKey: "AIzaSyCE3K9OYfqMC_VqwemutPb9P4144hcSXAM",
-  authDomain: "vue-practice4.firebaseapp.com",
-  projectId: "vue-practice4",
-  storageBucket: "vue-practice4.appspot.com",
-  messagingSenderId: "260978625580",
-  appId: "1:260978625580:web:77219dceff3a2c7bac9589"
-};
 firebase.initializeApp(firebaseConfig);
 
 new Vue({
