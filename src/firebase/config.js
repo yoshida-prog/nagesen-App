41366b4aa5e23b5610782f6fe4e04f37cc4b9972
よしだ <56685063+yoshida-prog@users.noreply.github.com>
2021-02-02 18:52:08 +0900

    Create config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export var firebaseConfig = {
+  apiKey: "AIzaSyCE3K9OYfqMC_VqwemutPb9P4144hcSXAM",
+  authDomain: "vue-practice4.firebaseapp.com",
+  projectId: "vue-practice4",
+  storageBucket: "vue-practice4.appspot.com",
+  messagingSenderId: "260978625580",
+  appId: "1:260978625580:web:77219dceff3a2c7bac9589"
+};
