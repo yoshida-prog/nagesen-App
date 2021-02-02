49272aa32f3e457f8c26854eadd8364db6ef6ffe
よしだ <56685063+yoshida-prog@users.noreply.github.com>
2021-02-02 18:50:50 +0900

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,32 +1,42 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import firebase from 'firebase'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     username: '',
-    Email: '',
+    email: '',
     password: ''
   },
   getters: {
     getUsername: state => state.username,
-    getEmail: state => state.Email,
+    getEmail: state => state.email,
     getPassword: state => state.password
   },
   mutations: {
-    updateUsername(state, value){
-      state.username = value
+    signUp({ email, password }) {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(user => {
+          alert('Create account: ' + user.user.email)
+        })
+        .catch(error => {
+          alert(error.message)
+        })
     },
-    updateEmail(state, value){
-      state.Email = value
+    updateUsername(state, username) {
+      state.username = username
     },
-    updatePassword(state, value){
+    updateEmail(state, value) {
+      state.email = value
+    },
+    updatePassword(state, value) {
       state.password = value
     }
   },
-  actions: {
-  },
-  modules: {
-  }
+  actions: {},
+  modules: {}
 })
