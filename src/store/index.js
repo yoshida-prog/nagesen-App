95531cb8ea533fd6711cdfc7c4c4332a41087040
よしだ <56685063+yoshida-prog@users.noreply.github.com>
2021-02-06 05:19:15 +0900

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import firebase from 'firebase'
+import db from '../main'
+import router from '../router'
 
 Vue.use(Vuex)
 
@@ -8,22 +10,26 @@ export default new Vuex.Store({
   state: {
     username: '',
     email: '',
-    password: ''
+    password: '',
+    balance: ''
   },
   getters: {
     getUsername: state => state.username,
     getEmail: state => state.email,
-    getPassword: state => state.password
+    getPassword: state => state.password,
+    getBalance: state => state.balance
   },
   mutations: {
-    createAccountAlert(state, email) {
-      alert('createAccount: ' + email)
-    },
-    loginAlart() {
-      alert('Success!')
+    rootingDashboard(state, { username, userId }) {
+      state.username = username
+      router.push({ name: 'Dashboard', params: { userId: userId } })
     },
     loginError(state, error) {
       alert(error)
+    },
+    setUserData(state, userData) {
+      state.username = userData.name
+      state.balance = userData.balance
     },
     updateUsername(state, value) {
       state.username = value
@@ -36,19 +42,45 @@ export default new Vuex.Store({
     }
   },
   actions: {
-    signUp({commit}, {email, password}) {
+    signUp({ commit }, { username, email, password }) {
       firebase
         .auth()
         .createUserWithEmailAndPassword(email, password)
-        .then(user => commit('createAccountAlert', user.user.email))
+        .then(function(user) {
+          db.collection('users').doc(user.user.uid).set({
+            name: username,
+            balance: 1000
+          }).then(() => {
+            commit('rootingDashboard', { username: username, userId: user.user.uid })
+          }).catch((error) => {
+            console.error('Error adding document: ', error)
+          })
+        })
         .catch(error => commit('loginError', error.message))
     },
-    signIn({commit}, {email, password}) {
+    signIn({ commit }, {email, password}) {
       firebase
         .auth()
         .signInWithEmailAndPassword(email, password)
-        .then(() => commit('loginAlart'))
+        .then(function(user){
+          db.collection('users').doc(user.user.uid).get()
+            .then((doc) => {
+              commit('rootingDashboard', { username: doc.data().name, userId: doc.id })
+            })
+            .catch(error => {
+              console.log('Error adding document: ', error)
+            })
+        })
         .catch(error => commit('loginError', error.message))
+    },
+    getUserDB({ commit }, userid){
+      db.collection('users').doc(userid).get()
+        .then((doc) => {
+          commit('setUserData', doc.data())
+        })
+        .catch(error => {
+          console.log('Error adding document: ', error)
+        })
     }
   },
   modules: {}
