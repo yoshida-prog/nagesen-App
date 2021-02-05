71bf127188bafe6a970e395a6e1bec601eff678b
よしだ <56685063+yoshida-prog@users.noreply.github.com>
2021-02-05 17:49:04 +0900

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -16,27 +16,14 @@ export default new Vuex.Store({
     getPassword: state => state.password
   },
   mutations: {
-    signUp({ email, password }) {
-      firebase
-        .auth()
-        .createUserWithEmailAndPassword(email, password)
-        .then(user => {
-          alert('Create account: ' + user.user.email)
-        })
-        .catch(error => {
-          alert(error.message)
-        })
+    createAccountAlert(state, email) {
+      alert('createAccount: ' + email)
     },
-    signIn({ email, password }) {
-      firebase
-        .auth()
-        .signInWithEmailAndPassword(email, password)
-        .then(() => {
-          alert('Success!')
-        })
-        .catch(error => {
-          alert(error.message)
-        })
+    loginAlart() {
+      alert('Success!')
+    },
+    loginError(state, error) {
+      alert(error)
     },
     updateUsername(state, value) {
       state.username = value
@@ -49,11 +36,19 @@ export default new Vuex.Store({
     }
   },
   actions: {
-    signUp(context, { email, password }) {
-      context.commit('signUp', { email, password })
+    signUp({commit}, {email, password}) {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(user => commit('createAccountAlert', user.user.email))
+        .catch(error => commit('loginError', error.message))
     },
-    signIn(context, { email, password }) {
-      context.commit('signIn', { email, password })
+    signIn({commit}, {email, password}) {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then(() => commit('loginAlart'))
+        .catch(error => commit('loginError', error.message))
     }
   },
   modules: {}
