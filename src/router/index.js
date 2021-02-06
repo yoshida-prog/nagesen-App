f244efde35d4b46b77a29b4c87b461cf08500fde
よしだ <56685063+yoshida-prog@users.noreply.github.com>
2021-02-06 22:29:43 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Signin from '../views/Signin.vue'
 import Signup from '../views/Signup.vue'
+import Dashboard from '../views/Dashboard.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
+    name: 'Signin',
+    component: Signin
+  },
+  {
+    path: '/signup',
     name: 'Signup',
     component: Signup
+  },
+  {
+    path: '/dashboard/:userId',
+    name: 'Dashboard',
+    component: Dashboard
   }
 ]
 
