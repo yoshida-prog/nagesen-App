17d8fd8c63a8f4b99dd3be54fe8fc9892db3266c
よしだ <56685063+yoshida-prog@users.noreply.github.com>
2021-02-11 21:45:15 +0900

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -145,38 +145,45 @@ export default new Vuex.Store({
         })
       })
     },
-    updateUserBalance({ state, commit }, getTransfer) {
-      //const isMyUid = firebase.auth().currentUser.uid
-      var isUidRef = db.collection('users').where('uid', '==', state.isUid)
-      //var isMyUidRef = db.collection('users').where('uid', '==', isMyUid)
-      db.runTransaction((transaction) => {
-        console.log(transaction)
-        // return transaction.get(isUidRef).then((docs) => {
-        return isUidRef.get().then((docs) => {
-          docs.forEach((doc) => {
-            db.collection('users').doc(doc.id).update({
-              balance: Number(state.isBalance) + Number(getTransfer)
-            })
+    //__________________________________________________________________________________________
+    // asyncで非同期処理をする関数を定義
+    // awaitはasync関数内でPromiseの結果が返されるまで待機する演算子
+    // つまりawait functionは、そのPromiseの結果が返されるまで待機する
+    // 今回の場合はupdateUserBalanceで2つの非同期処理を包括しているのでasync updateUserBalance()とすれば良い
+    // そして、結果が返ってくるまで待機が必要な関数は、送金する側される側のbalanceの書き換えであるので
+    // 2つのupdate関数にawaitをつける
+    // 2つのupdateが完了するとトランザクションの結果が返される
+    // 以上の流れで処理を終了する
+    //__________________________________________________________________________________________
+    // 非同期処理の宣言
+    async updateUserBalance({ state, commit }, getTransfer) {
+      const isMyUid = firebase.auth().currentUser.uid
+      // 全ての非同期処理が完了すればOKなのでtry-catch構文を用いる
+      try {
+        // クエリではtransactionは使えないらしいのでconsole.logを使って送金する側される側のidの格納場所を探した
+        const receiverDocs = await db.collection('users').where('uid', '==', state.isUid).get()
+        // console.log(receiverDocs)でちまちま探したところdocs.idにあった
+        const receiverId = receiverDocs.docs[0].id
+        const senderDocs = await db.collection('users').where('uid', '==', isMyUid).get()
+        const senderId = senderDocs.docs[0].id
+        // ドキュメントをgetしたらトランザクションスタート
+        await db.runTransaction(async transaction => {
+          // transactionのオペレーションはget,set,update,deleteなどで構成される
+          // 今回はupdateを使い、第一引数にドキュメント、第二引数に更新したいフィールドと値を渡す
+          // 送金される側
+          await transaction.update(db.collection('users').doc(receiverId), {
+            balance: firebase.firestore.FieldValue.increment(Number(getTransfer))
           })
           commit('updateDestinationBalance', getTransfer)
-        }).catch(error => {
-          console.log(error)
+          // 送金する側
+          await transaction.update(db.collection('users').doc(senderId), {
+            balance: firebase.firestore.FieldValue.increment(-Number(getTransfer))
+          })
+          commit('updateMyBalance', getTransfer)
         })
-        // return isMyUidRef.get().then((docs) => {
-        //   docs.forEach((doc) => {
-        //     db.collection('users').doc(doc.id).update({
-        //       balance: Number(state.balance) - Number(getTransfer)
-        //     })
-        //   })
-        //   commit('updateMyBalance', getTransfer)
-        // }).catch(function(error) {
-        //   console.log(error)
-        // })
-      }).then(() => {
-        console.log('ok')
-      }).catch((error) => {
+      } catch (error) {
         console.log(error)
-      })
+      }
     }
   },
   modules: {}
